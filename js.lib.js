761bfd6d2ac810c44c384ab2fb58f232e2b45b78
Aryan Khandal <aryankhandal0@gmail.com>
2018-02-03 13:55:19 +0530

    Yo

--- js.lib.js
+++ js.lib.js
@@ -5,15 +5,40 @@ let HelloLib=function(params) {
 }
 HelloLib.prototype.init=function () {
     Object.keys(this.params).map(function(inp){
-    	if(this.params[inp]==)
-    	console.log(input(this.params[inp].type,this.params[inp].placeholder))
+    	if(this.params[inp].type=="inputtext"){
+    		console.log(inputtext(this.params[inp].type,this.params[inp].placeholder,this.params[inp].class));
+    	} else if(this.params[inp].type=="submit"){
+        console.log(insbutton(this.params[inp].value,this.params[inp].class));
+    	} else if(this.params[inp].type=="dropd"){
+        console.log(dropdownfunc(this.params[inp].value,this.params[inp].class,this.params[inp].options))
+      }
     }.bind(this))
 }
-function inputtext(type,placeholder){
-	var element=document.createElement("input");
-	document.getElementsByTagName("BODY")[0].append(element);
-	document.getElementsByTagName("input")[0].append(linebreak);
+function inputtext(type,placeholder,clsName){
+	var element=document.createElement("INPUT");
+  document.getElementsByTagName("BODY")[0].append(linebreak);
+	document.body.append(element);
 	element.setAttribute("type",type);
 	element.setAttribute("placeholder",placeholder);
+  element.className += clsName;
+	return element;
+}
+function insbutton(value,clsName){
+	var element=document.createElement("BUTTON");
+  document.getElementsByTagName("BODY")[0].append(linebreak);
+  var data=document.createTextNode(value);
+  document.body.appendChild(element);
+  element.append(data);
+  element.className += clsName;
+	return element;
+}
+function dropdownfunc(value,clsName,options){
+	var element=document.createElement("SELECT");
+  document.body.appendChild(linebreak);
+  var data=document.createTextNode(value);
+	//document.getElementsByTagName("BODY")[0].append(element);
+  document.body.appendChild(element);
+  element.append(data);
+  element.className += clsName;
 	return element;
 }
